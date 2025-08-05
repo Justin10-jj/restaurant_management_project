4f383907ea764ac98fe4e5213f9721164ce558b4
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-05 17:06:30 +0530

    17934

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Order(models.Model):
+    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    total_amound=models.DecimalField(max_digits=8,decimal_places=2,default=0.00)
+    status=models.CharField(max_length=10,default=0)
+    created_at=models.DateTimeFeild(auto_now_add=True)
