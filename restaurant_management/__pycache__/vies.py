3d8364e3c90b1b1f8d343dca1859511617ee17d0
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-13 16:06:33 +0530

    30222

---
+++ restaurant_management/__pycache__/vies.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+def menu_list(request):
+    menu_list=[{
+        'name':'cheeseburger','description':'grilled beef patty with cheese,lectture,and tomato',
+        'price':10.99,
+        'is_avalibile':True
+    },
+    {
+        'name':'grilled chicken sandwitch',
+        'description':'chicken breast sandwich with mayo pickles',
+        'price':9.49,
+        'is_availbile':False
+            },]
+
+    return render(request,'menu/menu_list.html',{'menu_items':menu_items})
