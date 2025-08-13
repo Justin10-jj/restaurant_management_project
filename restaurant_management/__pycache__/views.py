5e80de10eb40b4915aea3f9f4c1d52a65619d873
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-13 16:34:37 +0530

    30225

--- restaurant_management/__pycache__/views.py
+++ restaurant_management/__pycache__/views.py
@@ -4,3 +4,10 @@ def homepage(request):
         'restaurant_name':restaurant_name if restaurant else"unmaned restaturant
     }
     return render(request,homepage.html,context)
+
+
+
+def home(request):
+    restaurant=Restaurant.objects.first()
+    phone_number=restaurant.phone if restaturant else 'not availabile'
+    return render(request,'home.html',{'phone_number':phone_number})
