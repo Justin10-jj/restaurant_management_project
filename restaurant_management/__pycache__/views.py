34c04427536716c6a1c12949cd35038b84a5dbbf
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-18 16:20:19 +0530

    34043

--- restaurant_management/__pycache__/views.py
+++ restaurant_management/__pycache__/views.py
@@ -11,3 +11,12 @@ def home(request):
     restaurant=Restaurant.objects.first()
     phone_number=restaurant.phone if restaturant else 'not availabile'
     return render(request,'home.html',{'phone_number':phone_number})
+
+
+def restaturant_list(request):
+    try:
+        restaturant=Restaurant.objects.all()
+        return render(request,'home.html',{'restaurant':restaturant})
+    except DatabaseError as e:
+        print(f"Database error occurred:{e}")
+        return HttpResponseServerError("An error ocuured while retrieving restaturants.Please try again later.")
