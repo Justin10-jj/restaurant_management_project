1ea61591083f22c1f62d8784c5a57523a63345cb
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-20 11:08:45 +0530

    35210

--- restaurant_management/__pycache__/models.py
+++ restaurant_management/__pycache__/models.py
@@ -2,4 +2,8 @@ class Profile(model.Models):
     full_name=models.CharField(max_length=20)
     email=models.EmailField(max_length=30)
     ph_number=modles.IntegerField(max_length=30)
+   
+class Feedbacks(models.Model):
+    comment=models.TextField()
+    submitted_at=models.DataTimeField(auto_now=True)
     
