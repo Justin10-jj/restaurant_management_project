67b7f1aad5b0d00d1b89f2aa5162f36e73cc6402
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-23 17:04:34 +0530

    37138

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     }
 }
 
+#customer settings
+RESTAURANT_NAME="Tasty Bites"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
