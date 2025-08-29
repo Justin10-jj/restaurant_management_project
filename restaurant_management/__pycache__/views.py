fcda19e36d375dbdbe0b37f236091f9451ceb4e6
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-29 15:07:55 +0530

    39857

--- restaurant_management/__pycache__/views.py
+++ restaurant_management/__pycache__/views.py
@@ -1,12 +1,3 @@
-def Contact_view(request):
-    if request.method=='POST':
-        if form.is_valid():
-            email=form.cleaned_data['email']
-            message=form.cleaned_data{'message'}
-
-            return render(request,'restaurantcontact.html',{'forms':form})
-
-
-def homepagephone(request):
-    restaurant=Restaurant.objects.all()
+def homepage(request):
+    restaurant=RestaurantLocation.objects.all()
     return render(request,'homepage.html',{'restaurant':restaurant})
