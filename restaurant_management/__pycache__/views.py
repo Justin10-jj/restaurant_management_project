3b3915bbd20a338938ce761d16b14e5a6f988597
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-29 15:32:08 +0530

    39855

--- restaurant_management/__pycache__/views.py
+++ restaurant_management/__pycache__/views.py
@@ -1,3 +1,13 @@
-def homepage(request):
-    restaurant=RestaurantLocation.objects.all()
-    return render(request,'homepage.html',{'restaurant':restaurant})
+from django.shortcuts imoprt render
+from.models import Restaurant
+
+def cart(request):
+    restaurant=MenuList.objects.all()
+    cart_item_count=0
+    if request.user.is_authentication:
+        cart=created=cart.objects.get_or_create(user=request.user)
+        cart_item_count=cart.total_item()
+
+    return render(request,'homepage.html',
+    {'restaurant':restaurant,
+    'cart_item_count':cart_item_count})
