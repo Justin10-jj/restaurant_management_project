65e3828c067f272ce2ca6373fc94cae3084e8541
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-08-29 14:13:57 +0530

    39854

--- restaurant_management/__pycache__/views.py
+++ restaurant_management/__pycache__/views.py
@@ -1,17 +1,7 @@
-from django.shortcuts import render
+def Contact_view(request):
+    if request.method=='POST':
+        if form.is_valid():
+            email=form.cleaned_data['email']
+            message=form.cleaned_data{'message'}
 
-def serachpage(request):
-    query=request.GET.get('search','')
-    if query:
-        menu_item=MenuItem.objects.filter(name_iconation=query)
-    else:
-        menu_item=MenuItem.objects.all()
-
-        location=MenuItem.objects.first()
-        context={
-            'restaurant_name':settings.RESTAURANT_NAME,
-            'location':location,
-            'menu_item':menu_item,
-            'query':query,
-        }
-        return render(request,'menulist.html',context)
+            return render(request,'restaurantcontact.html',{'forms':form})
