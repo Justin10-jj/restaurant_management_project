ee54e78b2b8c6f74e8ad7e488e3071a944156b4a
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-09-02 13:02:41 +0530

    41169

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -5,3 +5,22 @@ def Homes(request):
     info=RestaurantLocation.objects.all()
     phone=info.phone_number if info else "phone number unavailabile"
     return render(request,'homepage.html',{'restaurant_phone':phone})
+
+def homeee(request):
+    crumbs=[]
+    return render(request,'homepage.html',{
+        'crumbs':crumbs,
+        })
+
+def menu(request):
+    crumbs=[{'name':'menu','url':request.path},]
+    return render(request,'menu.html',{'crumbs':crumbs})
+
+
+
+def desserts(request):
+    crumbs=[
+        {'name':'menu','url':'/menu/'},
+        {'name':'Desserts'.'url'request.path}
+    ]
+    return render(request,'desserts.html',{'crumbs':crumbs})
