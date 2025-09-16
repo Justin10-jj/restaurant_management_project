f52fd25e0a9e6d63d88e818ebce86d705392a2a5
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-09-16 21:58:58 +0530

    44766

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def generate_coupon_view(request):
+    new_code=generate_coupon_code()
+    coupon=coupon.objects.create(code=new,discount=15.00)
+
+    return JsonResponse({
+        "message":"coupon generated successfully",
+        "coupon_code":coupon_code,
+        "discount":str(coupon.discount),    })
