d02c280986b83cb0284dcfb057b2c8c896917148
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-09-16 21:55:43 +0530

    44766

--- orders/models.py
+++ orders/models.py
@@ -7,3 +7,9 @@ class Order(models.Model):
     total_amound=models.DecimalField(max_digits=8,decimal_places=2,default=0.00)
     status=models.CharField(max_length=10,default=0)
     created_at=models.DateTimeFeild(auto_now_add=True)
+
+
+class coupon(models.Model):
+    code=models.CharField(max_length=20,unique=True)
+    discount=models.DecimalField(max_length=5,decimal_places=2,default=0.00)
+    active=models.BooleanField(default=True)
