2227ccab30e6f6ce6e0a1d4fc4164f6824d4bee8
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-09-29 18:15:23 +0530

    52056

--- orders/tests.py
+++ orders/tests.py
@@ -9,3 +9,22 @@ class OrderModeltests(TestCase):
         OrderItem.objects.create(order=order,menu_item=burger,quantity=2,price=burger.price)
         OrderItem.objects.create(order=order,menu_item=fries,quantity=3,price=fries.price)
         self.assertEqual(order.calculate_total(),Decimal("17.50"))
+
+
+
+class CancelOrderAPITests(APITestCase):
+    def setup(self):
+        self.user=User.objects.create_user(username="testuser",password="password123")
+        self.client.login(username="testuser",password="pass1234")
+        self.oredr=Order.objects.create(customer_name="test user")
+
+    def test_cancel_oredr_success(self):
+        response=self.client.delete(f"/oredrs/{self.oredr.id}/cancel/")
+        self.oredr.refresh_from_db()
+        self.asserEqual(response.status_code,status.HTTP_200_OK)
+        self.asserEqual(self.order.status,"CANCELLED")
+
+    def test_cancel_order_already_cancelled(self):
+        self.order.status='CANCELLED'
+        self.order.save()
+        response=self.client.delete("f/order/{self.order.id}/cancel/")
