a2def2cc86fde794a294ce423687bf1717ed99b7
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-09-29 18:46:48 +0530

    52054

--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class ContactFormSubmissionView(generic.CreateAPIView):
+    queryset=ContactFormSubmission.objects.all()
+    serializer_class=ContactFormSubmissionSerializer
+
+    def create(self,request,*args,**kwargs):
+        serializer=self.get_serializer(data=request.data)
+        if serializer.is_valid()
+        serializer.save()
+        return Response(
+            {"message":"ypur contact form has been submitted successfullt"
+
+            }
+        )
+    return Response(serializer.error)
