5f3bdb5c85b0576791ac93e0760e719c39352eff
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-09-29 17:45:55 +0530

    52057

--- orders/models.py
+++ orders/models.py
@@ -7,6 +7,14 @@ class Order(models.Model):
     total_amound=models.DecimalField(max_digits=8,decimal_places=2,default=0.00)
     status=models.CharField(max_length=10,default=0)
     created_at=models.DateTimeFeild(auto_now_add=True)
+    customer_name=models.CharField(max_length=255)
+
+
+    def calculated_total(models.Model):
+        total=Decimal("0.00")
+        for item in self.item.all():
+            total +=item.price * item.quantity
+        return total 
 
 
 class coupon(models.Model):
@@ -31,4 +39,14 @@ class Oredr(models.Model):
 
     objects=ActiveOrderManager()
 
-    
+class MenuItem(models.Model):
+    name=models.CharField(max_length=255)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=8,decimal_places=2)
+
+
+class OredrItem(models.Model):
+    order=models.ForeignKey(order,on_delete=models.CASCADE,related_name="item")
+    menu_item=models.ForeignKey(MenuItem,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    price=models.DecimalField(max_digits=8,decimal_places=2)
