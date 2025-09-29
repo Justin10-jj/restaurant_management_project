f5a7a34fd65a5fc555fd0f7860048b9419afe52a
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-09-29 17:51:25 +0530

    52057

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class OrderModeltests(TestCase):
+    def test_calculated_total(self):
+        burger=MenuItem.objects.create(name="Burger",price=Decimal("5.00"))
+        fries=MenuItem.objects.create(name="Fries",price=Decimal("2.50"))
+        order=Order.objects.create(customer_name="john Deo")
+        OrderItem.objects.create(order=order,menu_item=burger,quantity=2,price=burger.price)
+        OrderItem.objects.create(order=order,menu_item=fries,quantity=3,price=fries.price)
+        self.assertEqual(order.calculate_total(),Decimal("17.50"))
