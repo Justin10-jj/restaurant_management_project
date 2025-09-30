61d01e029b8bbe04298873b2205c930d987b0087
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-09-30 22:43:34 +0530

    52422

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digit=8,decimal_place=2)
+    is_daily_special=models.BooleanField(default=False)
+    
