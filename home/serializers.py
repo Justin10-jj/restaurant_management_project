0ccb81d5f233fd7d9d039b05e1996547fb09c44c
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-10-02 21:56:16 +0530

    52861

--- home/serializers.py
+++ home/serializers.py
@@ -9,3 +9,10 @@ class DailySpecialSerializer(serilazers.ModelSerializer):
     class Meta:
         model=MenuItem
         field=['id','name','description','price']
+
+
+class TableSerializer(serilaizers.ModelSerializer):
+    class Meta:
+        model=Table 
+        field=["table_number","capacity","is_availabile"]
+        
