f6b8d2f0f050e87165082eac6b0056e6f0b8d82c
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-10-02 22:05:51 +0530

    52860

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path=('daily-specials/',DailySpecialListView.as_View(),name='daily-specials'),
     path=("api/tables/availabile/",AvailaibleTableAPIView.as_view(),name="availabile_table_api"),
+    path("api/tables/<int:pk>",TableDetailAPIView.as_View(),name="table_detail_api"),
 ]
