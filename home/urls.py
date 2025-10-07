c9423a63fb8644a12b74b9bde7144ad806e42412
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-10-07 17:59:58 +0530

    53876

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path=("api/tables/availabile/",AvailaibleTableAPIView.as_view(),name="availabile_table_api"),
     path("api/tables/<int:pk>",TableDetailAPIView.as_View(),name="table_detail_api"),
     path("api/restaurant/status/",RestaurantStatusAPIView.as_view(),name="restaurant_status"),
+    path('',include(router.urls)),
 ]
