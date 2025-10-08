a2efb2211a163ab19e62b615d2ca09885ef47216
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-10-08 17:15:31 +0530

    53872

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("order/<int:pk>/total/",order_total_view,name="order-total"),
     path("order/<int:pk>/update-status/",OrderStatusUpdateView.as_view(),ame="order-status-update"),
     path("api/sales/daily/",DailySalesAPIView.as_view(),name="daily_sales_api"),
+    path('status/<int:oredr_id>/',views.get_order_status,name='get_order_status'),
 ]
