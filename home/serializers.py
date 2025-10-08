4b97348ff715f354f19551d12b9c632b8ea6f439
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-10-08 12:08:09 +0530

    53874

--- home/serializers.py
+++ home/serializers.py
@@ -22,3 +22,22 @@ class MenuCategorySerializers(serilaizers.ModelSerializer):
         model=MenuCategy 
         field=['id','name']
         
+
+class UserReviewSerualizer(Seializer.ModelSerializer):
+    user=serilaizers.StringRelatedField(read_only=True)
+    class Meta:
+        model=UserReview
+        fields=['id','user','menu_item','rating','comment','review_date']
+        read_only_fields=['id','user','review_date']
+
+
+    def Valodation_rating(self,value):
+        if not (1<=value<=5):
+            raise serilaizers.ValidationError("Rating must be between 1 and 5.")
+        return value
+
+
+    def craete(self,validatin_data):
+        request=self.context.get('request')
+        validated_data['user']=request.user
+        return super().craete(validated_data)
