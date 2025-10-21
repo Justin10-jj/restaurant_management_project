0ea781627df6d281c7f543e4b800599d5184b1c3
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-10-21 21:40:11 +0530

    55866

--- orders/serializers.py
+++ orders/serializers.py
@@ -13,24 +13,4 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Order
-        filed=["id","customer","item","total_price","created_at"]
-
-
-
-class OrderStatusUpdateSerializer(serializers.serializer):
-    class Meta:
-        model=Order
-        field=["status"]
-    def validate_status(self,value):
-        allowed_statuses=[choice[0]for choice in Order.STATUS.CHOICES]
-
-        if value not in allowed_statuses:
-            raise serializer.validationError(f"Invalid status.Allowed:{allowed_statuses}) 
-        return value    
-
-
-
-class CouponSerializer(serializer.ModelSerializer):
-    class Meta:
-        model=coupon 
-        field = ["code","discount_percentage"]
+        filed=["id","short_id","customer","item","total_price","created_at"]
