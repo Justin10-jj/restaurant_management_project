53c97fb8672a5ac57ed7bfe8aa9c34f9d4eb4c7d
Justin10-jj <justinjoyyeroor2002@gmail.com>
2025-10-27 21:54:42 +0530

    56391

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display=('name','address','phone_number','email')
+    search_field=('name','address')
+    list_filter=('is_active',)
